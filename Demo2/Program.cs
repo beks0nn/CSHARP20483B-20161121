48b4100071045fb1a5e03b134fc2ea2c55566daf
uffebjorklund <uffe@xsockets.net>
2016-11-21 10:13:02 +0100

    1st

---
+++ Demo2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo2
+{
+    // måste ärvas för att kunna skapa instans (abstrakt)
+    public abstract class Animal
+    {
+        public bool Carnivore { get; set; }
+
+        public Animal()
+        {
+            Console.WriteLine("CTOR - Animal");
+        }
+    }
+
+    // får inte ärva pga sealed
+    public sealed class CatAnimal : Animal
+    {
+        public CatAnimal()
+            : base()
+        {
+            Console.WriteLine("CTOR - CatAnimal");
+        }
+        //public new bool Carnivore { get; set; }
+    }
+
+    // gömd utanför assemblyn
+    internal class Lion : Animal
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Lion() { Carnivore = true };
+            //a.Carnivore = true;
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Demo3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo3
+{
+    public class Person
+    {
+        public int Age { get; protected set; }
+        public string Name { get; set; }
+
+        public void SetAge(int age)
+        {
+            this.Age = age;
+        }
+
+        //tostring is virtual on object, so we can override
+        public sealed override string ToString()
+        {
+            return string.Format("name: {0}, age: {1}", this.Name, this.Age);
+        }
+
+        //kan köra override i ärvande klasser
+        public virtual string Says()
+        {
+            return "Hello";
+        }
+    }
+    public class Customer : Person
+    {
+        public Customer()
+        {
+            this.Age = 34;
+        }
+        public override string Says()
+        {
+            return "To expensive";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person();
+            p.SetAge(34);
+            Console.WriteLine(p.ToString());
+            Console.WriteLine(p.Says());
+
+            var c = new Customer();
+            Console.WriteLine(c.Says());
+            Console.ReadLine();
+        }
+    }
+}
