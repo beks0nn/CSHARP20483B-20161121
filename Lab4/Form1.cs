c767f9c8e6b4d4e85d2d332e359fc15da3cd2585
uffebjorklund <uffe@xsockets.net>
2016-11-22 13:49:11 +0100

    events

--- Lab4/Form1.cs
+++ Lab4/Form1.cs
@@ -15,7 +15,14 @@ namespace Lab4
         delegate void ShowMessage(string m);
 
         ShowMessage myDelegate = (s) => {  };
-
+        ShowMessage anonymousA = (s) => 
+        { 
+            MessageBox.Show("Anonymous A" + s); 
+        };
+        ShowMessage anonymousB = (s) =>
+        {
+            MessageBox.Show("Anonymous B" + s);
+        };
         public Form1()
         {
             InitializeComponent();
@@ -33,21 +40,25 @@ namespace Lab4
         private void buttonDeleteDelA_Click(object sender, EventArgs e)
         {
             myDelegate -= ShowA;
+            myDelegate -= anonymousA;           
         }
 
         private void buttonAddDelA_Click(object sender, EventArgs e)
         {
             myDelegate += ShowA;
+            myDelegate += anonymousA;
         }
 
         private void buttonAddDelB_Click(object sender, EventArgs e)
         {
             myDelegate += ShowB;
+            myDelegate += anonymousB;
         }
 
         private void buttonDeleteDelB_Click(object sender, EventArgs e)
         {
             myDelegate -= ShowB;
+            myDelegate -= anonymousB;
         }
 
         private void buttonInvokeDelegate_Click(object sender, EventArgs e)
